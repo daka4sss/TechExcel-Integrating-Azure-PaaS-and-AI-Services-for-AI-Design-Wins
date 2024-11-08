038c4b0bfabf39d2e25eb385db89161b509aedeb
Daiki Kanemitsu <daka@microsoft.com>
2024-11-08 20:36:10 +0900

    fifth

--- src/ContosoSuitesWebAPI/Services/IVectorizationService.cs
+++ src/ContosoSuitesWebAPI/Services/IVectorizationService.cs
@@ -1,12 +1,10 @@
 ﻿using ContosoSuitesWebAPI.Entities;
 
-namespace ContosoSuitesWebAPI.Services
+namespace ContosoSuitesWebAPI.Services;
+public interface IVectorizationService
 {
-    public interface IVectorizationService
-    {
-        Task<float[]> GetEmbeddings(string text); 
+    Task<float[]> GetEmbeddings(string text); 
 
-    //Exercise 3 Task 3 TODO #1: Uncomment the following line of code to provide an execute a vector search method against Cosmos DB.
-        Task<List<VectorSearchResult>> ExecuteVectorSearch(float[] queryVector, int max_results = 0, double minimum_similarity_score = 0.8);
-    }
+//Exercise 3 Task 3 TODO #1: Uncomment the following line of code to provide an execute a vector search method against Cosmos DB.
+    Task<List<VectorSearchResult>> ExecuteVectorSearch(float[] queryVector, int max_results = 0, double minimum_similarity_score = 0.8);
 }
